2716214803d2262d3d639d4813e8df5111696b6b
Robert Geil <robert@rgeil.net>
2018-11-26 21:54:07 -0800

    Update App.js
    - Put in a base review class with title and review attributes

--- src/App.js
+++ src/App.js
@@ -2,24 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Review extends React.Component{
+
+  constructor() {
+    super();
+    this.state = {
+      movieTitle: "Sample Title",
+      movieReview:"Sample Review",
+      rating:0
+    };
+  }
+
+
+
+  render(){
+    const title = this.state.movieTitle;
+    const review = this.state.movieReview;
+    return(
+      <div>
+        <h2>{title}</h2>
+        <p>{review}</p>
+      </div>
+    )
+  }
+}
+
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Review/>
       </div>
     );
   }
