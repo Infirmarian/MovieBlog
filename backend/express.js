f0c25226c6d9080258ca12cc6c88becbcb1d03d2
Robert Geil <robert@rgeil.net>
2018-12-24 13:38:57 -0500

    Updated to firebase user credential storage
    - Connected to the firebase database
    - Updated endpoints
    - Setup index.html to register if logged in or not

--- backend/express.js
+++ backend/express.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const md5 = require("md5");
-const fs = require('fs');
+const firebase = require('./firebase.js');
 
 var bodyParser = require("body-parser");
 const app = express();
@@ -11,26 +11,52 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 
-app.post("/auth", (req, res) => {
+app.post("/auth/login", async (req, res) => {
     var hpwd = md5(req.body.password);
-    var un = req.body.username;
-
-    // load users from javascript file
-    var users = JSON.parse(fs.readFileSync('user.json', 'utf8'));
+    var un = req.body.username.toLowerCase();
     
-    if(un in users && users[un]["password"] === hpwd){
-        console.log("successfully logged in");
-        res.status(200).json({valid:true});
+    const result = await firebase.validUser(un, hpwd);
+    if(result === 0){
+        console.log(`User ${un} logged in at ${new Date().toString()}`);
+        // OAUTH
+        var token =  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        var exp = 3600;
+        firebase.addTempToken(token, un, exp);
+
+        res.status(200).json({
+            "access_token":token,
+            "token_type":"bearer",
+            "expires_in":exp,
+            "username":un
+        });
     }else{
-        console.log(req.body);
         console.log("Incorrect username and password");
-        res.status(200).json({valid:false});
+        res.status(403).json({"ErrorCode":"Invalid Request"});
     }
 });
 
-app.get("/index", (req, res)=>{
-    res.send("<h1>Yay, logged in!</h1>");
-})
+app.post("/auth/register", async (req, res)=>{
+    var hpwd = md5(req.body.password);
+    var un = req.body.username.toLowerCase();
+    try{
+        var result = await firebase.addUser(un, hpwd);
+        if(result != 0){
+            res.status(200).json({"ErrorStatus":result});
+        }else{
+            console.log(`Created user ${un} at ${new Date().toString()}`);
+            res.status(200).json({"ErrorStatus":0,"Success":true});
+        }
+    }catch(e){
+        console.log("Error registering user: "+e);
+    }
+});
+
+//logs a user out
+app.post("/auth/logout", async (req, res)=>{
+    var un = req.body.username;
+    await firebase.logout(un);
+    res.status(200).json({"logged out":true});
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`);
--- backend/public/login.js
+++ backend/public/login.js
@@ -1,7 +1,7 @@
 //login.js
 
 var form = document.getElementById("login");
-var submit = document.getElementById("submit");
+//var submit = document.getElementById("submit");
 
 //makes sure that the username and password aren't blank
 function checkBlank(){
@@ -12,14 +12,21 @@ function checkBlank(){
 form.onsubmit = function() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = () => {
-        if (xhttp.readyState == 4 && xhttp.status == 200){
-            if(! JSON.parse(xhttp.responseText).valid){
+        if (xhttp.readyState == 4){
+            if(xhttp.status == 403){
+                console.log(xhttp.responseText);
                 document.getElementById("wrongPassword").innerHTML = "Incorrect username or password!";
                 document.getElementById("password").value = "";
-            }else{
+            }else if(xhttp.status == 200){
                 // valid user
                 console.log("valid user");
-                document.location.href = "index";
+                var resp = JSON.parse(xhttp.responseText);
+                //expiration date functions
+                var d = new Date();
+                d.setTime(d.getTime()+(resp.expires_in*1000));
+                document.cookie = `token=${resp.access_token};expires=${d.toGMTString()};path=/;`
+                document.cookie = `username=${resp.username};expires=${d.toGMTString()};path=/;`
+                document.location.href = "index.html";
             }
         }
       };
@@ -31,9 +38,29 @@ form.onsubmit = function() {
     }else{
         var username = document.getElementById("username").value;
         var password = document.getElementById("password").value;
-        xhttp.open("POST", "/auth", true);
+        xhttp.open("POST", "/auth/login", true);
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhttp.send(`username=${username}&password=${password}`);
         return false;
     }
 };
+
+function getCookie(cookieName){
+    const co = document.cookie;
+    var split = co.split(";");
+    for(var i = 0; i<split.length; i++){
+        var c = split[i];
+        //trim whitespace
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(cookieName) == 0) {
+          return c.substring(cookieName.length+1, c.length);
+        }
+    }
+    //no cookies found
+    return null;
+}
+if(getCookie("token") !== null){
+    document.location.href = "index.html";
+}
